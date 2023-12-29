5e409c6bf453cf7b8e49915379c81d0531455dd6
Dot Matrix <domatrix6@gmail.com>
2023-12-29 17:36:02 -0500

    Added base Chaos Mod project to solution, confirmed able to build.

---
+++ LC-ChaosMod/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LC_ChaosMod
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static bool loaded;
+        public static ManualLogSource LogSource;
+        private readonly Harmony _harmony = new(MyPluginInfo.PLUGIN_GUID);
+
+
+        public void Awake()
+        {
+            LogSource = BepInEx.Logging.Logger.CreateLogSource(MyPluginInfo.PLUGIN_NAME);
+
+            LogSource.LogInfo("Loaded Chaos Mod & applying patches.");
+            _harmony.PatchAll(typeof(Plugin).Assembly);
+        }
+    }
+}

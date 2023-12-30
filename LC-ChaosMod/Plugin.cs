f14b6626ebde16152cd08abfc05f41ca1e9e6fb2
Dot Matrix <domatrix6@gmail.com>
2023-12-30 14:14:56 -0500

    Added Plugin Config

--- LC-ChaosMod/Plugin.cs
+++ LC-ChaosMod/Plugin.cs
@@ -8,18 +8,31 @@ namespace LC_ChaosMod
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public static bool loaded;
-        public static ManualLogSource? LogSource;
         private readonly Harmony _harmony = new(MyPluginInfo.PLUGIN_GUID);
 
         internal static ManualLogSource LogSource = BepInEx.Logging.Logger.CreateLogSource(MyPluginInfo.PLUGIN_NAME);
+        internal static PluginConfig? PluginConfig { get; private set; }
+
         //! Plugin startup logic
         public void Awake()
         {
-            LogSource = BepInEx.Logging.Logger.CreateLogSource(MyPluginInfo.PLUGIN_NAME);
+
+            PluginConfig ??= new(base.Config);
+            PluginConfig.InitBindings();
+
+            if (!PluginConfig.Plugin_Enabled)
+            {
+                LogSource.LogWarning("Chaos Mod loaded, but has been disabled. Not applying patches.");
+                return;
+            }
 
             LogSource.LogInfo("Loaded Chaos Mod & applying patches.");
             _harmony.PatchAll(typeof(Plugin).Assembly);
+        }
+
+        public void OnDestroy()
+        {
+
         }
     }
 }
---
+++ LC-ChaosMod/PluginConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using BepInEx.Configuration;
+
+namespace LC_ChaosMod
+{
+    [Serializable]
+    internal class PluginConfig
+    {
+        private readonly ConfigFile _configFile;
+
+        internal bool Plugin_Enabled { get; set; }
+
+        internal PluginConfig(ConfigFile configFile)
+        {
+            _configFile = configFile;
+        }
+
+        private T ConfigEntry<T>(string section, string key, T defaultValue, string description)
+            => _configFile.Bind(section, key, defaultValue, description).Value;
+
+        internal void InitBindings()
+        {
+            var mainSection = "Main";
+            Plugin_Enabled = ConfigEntry(mainSection, "Enable Mod", true, "Turns this mod on or off.");
+        }
+    }
+}
